daa573e36336b0d5ddce75772562ec1ac42a888a
chikpan <chikpanwong@csus.edu>
2024-11-01 19:45:02 -0700

    testing login page website codeing

--- tutortitans/src/App.js
+++ tutortitans/src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import backButton from "./assets/images/BackButton.png";
+import greenRectangleDesign from "./assets/images/greenbox.png";
+import logo from "./assets/images/logo.svg";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const StudentLoginPage = () => {
+    return (
+        <div className="student-login-page">
+            <div className="div">
+                <div className="overlap-group">
+                    <img
+                        className="green-rectangle"
+                        alt="Green rectangle"
+                        src={greenRectangleDesign}
+                    />
 
-export default App;
+                    <div className="sign-in-box">
+                        <div className="div-2">
+                            <div className="frame">
+                                <div className="text-wrapper">Student Login</div>
+
+                                <div className="frame-2">
+                                    <div className="frame-3">
+                                        <div className="username-or-email-wrapper">
+                                            <div className="username-or-email">JohnDoe@gmail.com</div>
+                                        </div>
+
+                                        <div className="div-wrapper">
+                                            <div className="text-wrapper-2">********</div>
+                                        </div>
+                                    </div>
+
+                                    <div className="sign-in-button">
+                                        <div className="text-wrapper-3">Sign in</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="text-wrapper-4">Forgot your password?</div>
+                        </div>
+
+                        <div className="frame-wrapper">
+                            <div className="frame-4">
+                                <div className="rectangle" />
+
+                                <div className="text-wrapper-5">Tutor Login</div>
+
+                                <div className="rectangle" />
+                            </div>
+                        </div>
+
+                        <div className="div-2">
+                            <p className="text-wrapper">No account? Sign up as a:</p>
+
+                            <div className="sign-up-buttons">
+                                <div className="div-wrapper-2">
+                                    <div className="text-wrapper-6">Student</div>
+                                </div>
+
+                                <div className="div-wrapper-2">
+                                    <div className="text-wrapper-6">Tutor</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="top-bar">
+                    <div className="back-logo">
+                        <img className="logo" alt="Logo" src={logo} />
+
+                        <img className="back-button" alt="Back button" src={backButton} />
+                    </div>
+
+                    <p className="TUTOR-TITANS">
+                        <span className="span">TUTOR</span>
+
+                        <span className="text-wrapper-7">&nbsp;</span>
+
+                        <span className="text-wrapper-8">TITANS</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
