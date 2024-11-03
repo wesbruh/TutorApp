618b9fb52afdaa011e7dd391c7d40645e1e2dd06
chikpan <chikpanwong@csus.edu>
2024-11-02 21:08:35 -0700

    add server

---
+++ BackEnd/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+
+//configure env
+dotenv.config();
+
+//rest object
+const app = express();
+
+//rest api
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome</h1>");
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app. listen (PORT, () => {
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+});
