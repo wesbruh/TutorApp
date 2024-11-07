c83540a0c1cb9d1e83bf3fd41c33e878de51f1af
sb11-1 <sbajwa2@csus.edu>
2024-11-06 20:17:44 -0800

    This is a test

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-
+//this is a test Sim
 //configure env
 dotenv.config();
 
